1918c335158064dc7b86d5558e1708e95da0d8a4
Maru32768 <maru32768@gmail.com>
2023-01-28 19:36:23 +0900

    fix: configファイル更新時に名前が変更されない

--- src/main/java/net/kunmc/lab/alias/config/Config.java
+++ src/main/java/net/kunmc/lab/alias/config/Config.java
@@ -1,13 +1,23 @@
 package net.kunmc.lab.alias.config;
 
+import net.kunmc.lab.alias.alias.AliasOperation;
 import net.kunmc.lab.configlib.BaseConfig;
 import net.kunmc.lab.configlib.value.map.UUID2StringMapValue;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 public class Config extends BaseConfig {
     // UUID, Player名のaliasを保持
-    public UUID2StringMapValue playerAlias = new UUID2StringMapValue();
+    public UUID2StringMapValue playerAlias = new UUID2StringMapValue().onModify(x -> {
+        x.forEach((k, v) -> {
+            Player p = Bukkit.getPlayer(k);
+            if (p != null) {
+                AliasOperation.setPlayerName(p, v);
+            }
+        });
+    });
 
     public Config(@NotNull Plugin plugin) {
         super(plugin);
