f80095675d4cacbb557899c95bca5c44a96be4c2
Maru32768 <maru32768@gmail.com>
2023-01-28 20:44:54 +0900

    feat: setnameコマンドをエイリアス名でも叩けるように

--- src/main/java/net/kunmc/lab/alias/command/SetNameCommand.java
+++ src/main/java/net/kunmc/lab/alias/command/SetNameCommand.java
@@ -2,6 +2,8 @@ package net.kunmc.lab.alias.command;
 
 import net.kunmc.lab.alias.Alias;
 import net.kunmc.lab.alias.alias.AliasOperation;
+import net.kunmc.lab.alias.config.Config;
+import net.kunmc.lab.alias.util.AliasUtil;
 import net.kunmc.lab.commandlib.Command;
 import net.kunmc.lab.commandlib.argument.PlayerArgument;
 import net.kunmc.lab.commandlib.argument.StringArgument;
@@ -9,8 +11,17 @@ import net.kunmc.lab.commandlib.argument.StringArgument;
 public class SetNameCommand extends Command {
     public SetNameCommand() {
         super("setname");
-       
-        argument(new PlayerArgument("target"), new StringArgument("name"), (player, name, ctx) -> {
+        Config config = Alias.getPlugin().config;
+
+        argument(new PlayerArgument("target", option -> {
+            option.suggestionAction(sb -> {
+                      config.playerAlias.values()
+                                        .forEach(sb::suggest);
+                  })
+                  .additionalParser((ctx, input) -> {
+                      return AliasUtil.getPlayerFromAlias(input);
+                  });
+        }), new StringArgument("name"), (player, name, ctx) -> {
             Alias.getPlugin().config.playerAlias.put(player.getUniqueId(), name);
             AliasOperation.setPlayerName(player, name);
 
