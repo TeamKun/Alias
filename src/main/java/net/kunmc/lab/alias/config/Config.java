2d286b92e2e45a38f5e2b9fa3dd8d23d4605dec6
Maru32768 <maru32768@gmail.com>
2023-01-28 19:59:10 +0900

    fix: reloadすると名前が元に戻ったままになる

--- src/main/java/net/kunmc/lab/alias/config/Config.java
+++ src/main/java/net/kunmc/lab/alias/config/Config.java
@@ -4,20 +4,19 @@ import net.kunmc.lab.alias.alias.AliasOperation;
 import net.kunmc.lab.configlib.BaseConfig;
 import net.kunmc.lab.configlib.value.map.UUID2StringMapValue;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 public class Config extends BaseConfig {
     // UUID, Player名のaliasを保持
-    public UUID2StringMapValue playerAlias = new UUID2StringMapValue().onModify(x -> {
-        x.forEach((k, v) -> {
-            Player p = Bukkit.getPlayer(k);
-            if (p != null) {
-                AliasOperation.setPlayerName(p, v);
-            }
-        });
-    });
+    public UUID2StringMapValue playerAlias = new UUID2StringMapValue().onInitialize(x -> {
+                                                                          Bukkit.getOnlinePlayers()
+                                                                                .forEach(AliasOperation::applyConfigPlayerName);
+                                                                      })
+                                                                      .onModify(x -> {
+                                                                          Bukkit.getOnlinePlayers()
+                                                                                .forEach(AliasOperation::applyConfigPlayerName);
+                                                                      });
 
     public Config(@NotNull Plugin plugin) {
         super(plugin);
